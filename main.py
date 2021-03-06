6fcb563021cc9064489fcd8212642ef75f14e0b6
ShohanurRahmanShohan <78851871+ShohanurRahmanShohan@users.noreply.github.com>
2021-03-06 22:05:59 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+print("~ Hyper Repeater~ ©Shohan©")
+
+print(" ******************")
+
+text=input("Text you wanted to Repeat : ")
+
+print(" ******************")
+
+number=int(input("Your repeat amount :  "))
+
+print(" ******************")
+
+i= 0 
+
+print("√√√√√√√√√√√")
+
+while i<number:
+
+	print (text)	i=i+1
+
+print("✓✓™™™✓✓✓")
+
+print ("^ © Shohanur Rahman© ^")
